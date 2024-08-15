b070f7cf34404a5eaedf34e53154b7e29b5783fc
Paradox Shadow <testa7294@gmail.com>
2024-08-15 11:58:54 -0700

    Media queries

--- main.js
+++ main.js
@@ -1,18 +1,17 @@
-function EasyPeasyParallax() {
-    scrollPos = $(this).scrollTop();
-    $('.parallax.first').css({
-        'background-position': '50% ' + (-scrollPos / 4) + "px"
-    });
-    $('.parallax.second').css({
-        'background-position': '50% ' + (-scrollPos / 4) + "px"
-    });
-    $('.parallax.third').css({
-        'background-position': '50% ' + (-scrollPos / 4) + "px"
-    });
-}
+const navLinks = document.querySelectorAll('.navigation-list a');
+navLinks.forEach(link => {
+    link.addEventListener('click', function(event) {
+        //Prevent the default action
+        event.preventDefault();
 
-$(document).ready(function () {
-    $(window).scroll(function () {
-        EasyPeasyParallax();
+        //Get the target section id from the href attribute
+        const targetID = this.getAttribute('href').substring(1);
+
+        //Get the target section element
+        const targetSection = document.getElementById(targetID);
+
+        //Scroll to the target section smoothly
+        targetSection.scrollIntoView({behavior: 'smooth'});
+
     });
 });
